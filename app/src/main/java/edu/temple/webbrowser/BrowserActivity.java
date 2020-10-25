0405059ab560e2c0165117b23b15493bda7db6ff
sarahparoya <sarahparoya@gmail.com>
2020-10-25 16:13:27 -0400

    Initial commit

---
+++ app/src/main/java/edu/temple/webbrowser/BrowserActivity.java
@@ -0,0 +1,15 @@
+package edu.temple.webbrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BrowserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
