7c94aca180a7575d301bb1a1f1e4d8d1faa2302f
sarahparoya <sarahparoya@gmail.com>
2020-11-10 03:25:25 -0500

    Commit: Starting the new layouts for landscape/portrait mode. Still need to set them up though lols

--- app/src/main/java/edu/temple/webbrowser/PageViewerFragment.java
+++ app/src/main/java/edu/temple/webbrowser/PageViewerFragment.java
@@ -23,6 +23,8 @@ public class PageViewerFragment extends Fragment {
 
     WebView WebBrowser;
 
+    //hehehe
+
     String reqStr = "https://";
 
     ArrayList<String> urlStack = new ArrayList<String>();
