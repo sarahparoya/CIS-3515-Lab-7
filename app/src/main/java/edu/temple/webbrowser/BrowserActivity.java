88fc541821cb705bc923d41dad7142cfbac011f8
sarahparoya <sarahparoya@gmail.com>
2020-11-10 23:13:32 -0500

    Commit: Activity layouts made, still need to make the fragment layout, essentially still setting up the app omg rip </3

--- app/src/main/java/edu/temple/webbrowser/BrowserActivity.java
+++ app/src/main/java/edu/temple/webbrowser/BrowserActivity.java
@@ -19,61 +19,13 @@ import android.widget.ImageButton;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BrowserActivity extends AppCompatActivity implements PageControlFragment.SendURLInterface, PageControlFragment.goBackInterface, PageControlFragment.goNextInterface, PageViewerFragment.SetURLInterface {
-    static PageControlFragment PageControlFragment = new PageControlFragment();
-    static PageViewerFragment PageViewerFragment = new PageViewerFragment();
+public class BrowserActivity extends AppCompatActivity {
 
-    PageControlFragment pageControlFragment;
-    PageViewerFragment pageViewerFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Browser Activity");
 
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
-
-        pageControlFragment = new PageControlFragment();
-        pageViewerFragment = new PageViewerFragment();
-
-        FragmentManager FM = getSupportFragmentManager();
-
-        FragmentTransaction FT = FM.beginTransaction();
-        FT.add(R.id.page_control, pageControlFragment);
-        FT.add(R.id.page_viewer_fragment, pageViewerFragment);
-        FT.commit();
-    }
-
-    public void SendURL(String URL){
-        String reqStr = "https://";
-
-
-
-
-        if(!URL.substring(0,reqStr.length()).equals(reqStr)){
-            URL = reqStr.concat(URL);
-        }
-
-        pageViewerFragment.setURL(URL);
-
-        pageControlFragment.setText(pageViewerFragment.getURL());
-
-
-    }
-
-    @Override
-    public void SetURL() {
-
-        pageControlFragment.setText(pageViewerFragment.getURL());
-    }
-
-    @Override
-    public void goBack() {
-        pageViewerFragment.goBack();
-    }
-
-    public void goNext() {
-        pageViewerFragment.goNext();
     }
 }
